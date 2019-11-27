fd3fde36c561c059ef59610c8fa5132a0c5cc5da
Keion Peters <peterskt@umich.edu>
2019-11-27 06:00:00 -0500

    commit of testdup testMixed

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -52,6 +52,52 @@ public class testSelectionSort extends TestCase {
 
 
 	    }
+      public void testMixed(){
+            int[] arr = new int[5];
+              arr[0] = 8;
+              arr[1] = 9;
+              arr[2] = -7;
+              arr[3] = 10;
+              arr[4] = -2;
+
+              int[] Sortedarr = new int[5];
+              Sortedarr[0] = -7;
+              Sortedarr[1] = -2;
+              Sortedarr[2] = 8;
+              Sortedarr[3] = 9;
+              Sortedarr[4] = 10;
+
+            /** Test data contains with both positive, negative and zeros **/
+              SelectionSort object=new SelectionSort();
+              object.basicSelectionSort(arr);
+
+              for(int i=0;i<arr.length;i++)
+              {assertEquals("Fails",arr[i],Sortedarr[i]);}
+          }
+
+          public void testDuplicates(){
+       int[] arr = new int[5];
+         arr[0] = 8;
+         arr[1] = 8;
+         arr[2] = 7;
+         arr[3] = 10;
+         arr[4] = 2;
+
+         int[] Sortedarr = new int[5];
+         Sortedarr[0] = 2;
+         Sortedarr[1] = 7;
+         Sortedarr[2] = 8;
+         Sortedarr[3] = 8;
+         Sortedarr[4] = 10;
+
+
+         /** Test data contains duplicates **/
+         SelectionSort object=new SelectionSort();
+         object.basicSelectionSort(arr);
+         for(int i=0;i<arr.length;i++)
+         {assertEquals("Fails",arr[i],Sortedarr[i]);}
+
+     }
 
 
 
